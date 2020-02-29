f58e6a1037f43c6f4fa2085f5fc53a60c7cfe495
EC2 Default User <ec2-user@ip-172-31-43-159.us-east-2.compute.internal>
2020-02-29 17:24:12 +0000

    building out google auth

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-#from config import Config
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #from flask_bootstrap import Bootstrap
 
 #initiatlize app
 app = Flask(__name__)
-#app.config.from_object(Config)
+app.config.from_object(Config)
 
 #initialize extensions
-#db = SQLAlchemy(app)
-#migrate = Migrate(app, db, compare_type=True) #compare_type=true picks up on column type changes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, compare_type=True) #compare_type=true picks up on column type changes
 #bootstrap = Bootstrap(app)
 
 #imports at bottom to avoid circular imports
---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'bbbrrrrr-apppp'
+    
+    #db setup
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    #google auth setup
+    GOOGLE_OAUTH_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_CLIENT_ID")
+    GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET")
