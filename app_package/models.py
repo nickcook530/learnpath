230d5d076c3cd48ba48f0da944d0027fd62f2b3d
EC2 Default User <ec2-user@ip-172-31-43-159.us-east-2.compute.internal>
2020-03-07 21:59:21 +0000

    created test path and steps and displayed them

--- app_package/models.py
+++ app_package/models.py
@@ -1,4 +1,5 @@
 from app_package import db, login_manager
+from datetime import datetime
 from flask_login import LoginManager, UserMixin
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
 
@@ -7,9 +8,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), index=True, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
+    paths = db.relationship('Path', backref='creator', lazy='dynamic')
+    steps = db.relationship('Step', backref='creator', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.name)  
+        return '<User {}>'.format(self.email)  
         
 class OAuth(OAuthConsumerMixin, db.Model):
     provider_user_id = db.Column(db.String(256), unique=True, nullable=False)
@@ -20,3 +23,30 @@ class OAuth(OAuthConsumerMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+    
+    
+class Path(db.Model):
+    __tablename__ = 'path'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=False)
+    description = db.Column(db.String(400), index=True, unique=False)
+    is_public = db.Column(db.Boolean, default=True, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    steps = db.relationship('Step', backref='path', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Path {}>'.format(self.name)
+
+class Step(db.Model):
+    __tablename__ = 'step'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False)
+    description = db.Column(db.String(400), unique=False)
+    link = db.Column(db.String(2048), unique=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    path_id = db.Column(db.Integer, db.ForeignKey('path.id'))
+
+    def __repr__(self):
+        return '<Step {}>'.format(self.name)  
--- app_package/routes.py
+++ app_package/routes.py
@@ -1,16 +1,25 @@
 from app_package import app
+from app_package.models import Path, Step
 from flask import redirect, url_for, flash, render_template
-from flask_login import login_required, logout_user
+from flask_login import login_required, logout_user, current_user
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template("base.html")
+def home():
+    paths = []
+    if current_user.is_authenticated:
+        paths = Path.query.filter_by(user_id=current_user.id).all()
+    return render_template("homescreen.html", paths=paths)
+
+@app.route('/<user_id>/paths/<path_id>')
+def path(user_id, path_id):
+    path = Path.query.filter_by(user_id=user_id, id=path_id).first_or_404()
+    steps = path.steps.all()
+    return render_template("path.html", path=path, steps=steps)
     
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     flash("You have logged out")
-    return redirect(url_for("index"))
+    return redirect(url_for("home"))
