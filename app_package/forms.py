41ffadb9b792fc0d452ffe51945b1553c5076b73
Nick Cook <nickcook530@gmail.com>
2020-03-14 13:08:58 -0400

    create new step for existing path

--- app_package/forms.py
+++ app_package/forms.py
@@ -6,3 +6,9 @@ class PathForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired(), Length(max=120)])
     description = StringField('Description', validators=[InputRequired(), Length(max=400)])
     submit = SubmitField('Save')
+
+class StepForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired(), Length(max=120)])
+    description = StringField('Description', validators=[InputRequired(), Length(max=400)])
+    link = StringField('Link', validators=[Length(max=2048)])
+    submit = SubmitField('Save')
--- app_package/routes.py
+++ app_package/routes.py
@@ -1,6 +1,6 @@
 from app_package import app, db
 from app_package.models import Path, Step
-from app_package.forms import PathForm
+from app_package.forms import PathForm, StepForm
 from flask import redirect, url_for, flash, render_template
 from flask_login import login_required, logout_user, current_user
 
@@ -19,12 +19,21 @@ def home():
         paths = Path.query.filter_by(user_id=current_user.id).all()
     return render_template("homescreen.html", paths=paths, current_user=current_user, path_form=path_form)
 
-@app.route('/<user_id>/paths/<path_id>')
+@app.route('/<user_id>/paths/<path_id>', methods=['GET', 'POST'])
 def path(user_id, path_id):
-    path_form = PathForm() #consider removing this?
+    path_form = PathForm() #consider removing this from base?
+    step_form = StepForm()
     path = Path.query.filter_by(user_id=user_id, id=path_id).first_or_404()
     steps = path.steps.all()
-    return render_template("path.html", path=path, steps=steps, path_form=path_form)
+    if step_form.validate_on_submit():
+        new_step = Step(name=step_form.name.data, description=step_form.description.data, link=step_form.link.data, 
+            path=path, creator=current_user)
+        db.session.add(new_step)
+        db.session.commit()
+        flash('SUCCESS')
+        return redirect(url_for('path', user_id=current_user.id, path_id=path.id))
+    return render_template("path.html", path=path, steps=steps, creator_id=int(user_id), 
+        current_user=current_user, path_form=path_form, step_form=step_form)
     
 @app.route("/logout")
 @login_required
