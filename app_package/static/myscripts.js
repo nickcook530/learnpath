6076b33f28378be221b6dc503487971149653d8b
Nick Cook <nickcook530@gmail.com>
2020-03-20 16:46:42 -1000

    Refactor step and path modal naming (still need to test locally)

--- app_package/static/myscripts.js
+++ app_package/static/myscripts.js
@@ -1,18 +1,18 @@
 $('.step-options').on('click', ".edit-step", function() {
     const card = $(this).closest(".step-card");
-    document.querySelector('#newStep #stepid').value = card.data("stepid");
-    document.getElementById("newStepLabel").innerHTML = "Edit Step";
-    document.querySelector('#newStep #name').value = card.find(".card-title")[0].innerHTML;
-    document.querySelector('#newStep #description').value = card.find(".card-text")[0].innerHTML;
-    document.querySelector('#newStep #link').value = card.find(".step-link")[0].href;
-    $('#newStep').modal('show');
+    document.querySelector('#stepModal #stepid').value = card.data("stepid");
+    document.getElementById("#stepModalLabel").innerHTML = "Edit Step";
+    document.querySelector('#stepModal #name').value = card.find(".card-title")[0].innerHTML;
+    document.querySelector('#stepModal #description').value = card.find(".card-text")[0].innerHTML;
+    document.querySelector('#stepModal #link').value = card.find(".step-link")[0].href;
+    $('#stepModal').modal('show'); //Is this redundant?
 });
 
 $('#new-step-btn').on('click', function() {
-    document.querySelector('#newStep #stepid').value = "";
-    document.getElementById("newStepLabel").innerHTML = "New Step";
-    document.querySelector('#newStep #name').value = "";
-    document.querySelector('#newStep #description').value = "";
-    document.querySelector('#newStep #link').value = "";
-    $('#newStep').modal('show');
+    document.querySelector('#stepModal #stepid').value = "";
+    document.getElementById("#stepModalLabel").innerHTML = "New Step";
+    document.querySelector('#stepModal #name').value = "";
+    document.querySelector('#stepModal #description').value = "";
+    document.querySelector('#stepModal #link').value = "";
+    $('#stepModal').modal('show');
 });
